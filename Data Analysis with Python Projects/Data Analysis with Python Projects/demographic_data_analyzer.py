ec103e6d600f3afa32c8b4f70c9e0dc91145c3f0
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-08-05 15:37:27 -0700

    add other data analysis projects

--- Data Analysis with Python Projects/Data Analysis with Python Projects/demographic_data_analyzer.py
+++ Data Analysis with Python Projects/Data Analysis with Python Projects/demographic_data_analyzer.py
@@ -3,42 +3,52 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df['sex'] == "Male"].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelors_num = len(df[df['education'] == 'Bachelors'])
+    total_num = len(df)
+    percentage_bachelors = round(bachelors_num / total_num * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df[df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
+    lower_education = df[~df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_salary_num = len(higher_education[higher_education.salary == ">50K"])
+    lower_salary_num = len(lower_education[lower_education.salary == ">50K"])
+
+    higher_education_rich = round(higher_salary_num / len(higher_education) * 100, 1)
+    lower_education_rich = round(lower_salary_num / len(lower_education) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df["hours-per-week"] == min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = round(len(num_min_workers[num_min_workers.salary == ">50K"]) / len(num_min_workers) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    country_count = df['native-country'].value_counts()
+    country_rich_count = df[df['salary'] == '>50K']['native-country'].value_counts()
+
+    highest_earning_country = (country_rich_count / country_count * 100).idxmax()
+    highest_earning_country_percentage = round((country_rich_count / country_count * 100).max(), 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    india_df = df[df['native-country'] == "India"]
+    occupation_count = india_df[india_df['salary'] == ">50K"]['occupation'].value_counts()
+    top_IN_occupation = occupation_count.idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
--- Data Analysis with Python Projects/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Data Analysis with Python Projects/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
+    else:
+        temp = np.array(list).reshape(3, 3)
 
-
-
-
+    calculations = {
+        'mean': [temp.mean(axis=0).tolist(), temp.mean(axis=1).tolist(), temp.mean()],
+        'variance': [temp.var(axis=0).tolist(), temp.var(axis=1).tolist(), temp.var()],
+        'standard deviation': [temp.std(axis=0).tolist(), temp.std(axis=1).tolist(), temp.std()],
+        'max': [temp.max(axis=0).tolist(), temp.max(axis=1).tolist(), temp.max()],
+        'min': [temp.min(axis=0).tolist(), temp.min(axis=1).tolist(), temp.min()],
+        'sum': [temp.sum(axis=0).tolist(), temp.sum(axis=1).tolist(), temp.sum()]
+    }
     return calculations
