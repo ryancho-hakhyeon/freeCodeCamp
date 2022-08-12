483a26f72fff4769df1eb2c51375cdb3e24d7567
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-08-11 22:27:44 -0700

    few things

--- Data Analysis with Python Projects/Data Analysis with Python Projects/demographic_data_analyzer.py
+++ Data Analysis with Python Projects/Data Analysis with Python Projects/demographic_data_analyzer.py
@@ -9,7 +9,7 @@ def calculate_demographic_data(print_data=True):
     race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = df[df['sex'] == "Male"].mean().round(1)
+    average_age_men = round(df[df['sex'] == "Male"].age.mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
     bachelors_num = len(df[df['education'] == 'Bachelors'])
